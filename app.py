a990592d01d9d6ea170c9f555b6728b6ec23fd35
gauniyal rakesh <gauniyal.rakesh@bcg.com>
2019-12-06 15:48:00 +0530

    Pushing base app.

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '127.0.0.1', port = 5000)
+    from gevent.pywsgi import WSGIServer
+    app.debug = True
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
